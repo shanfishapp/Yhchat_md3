ee1ff9a3916b5bdb1892ce8a87a87e655fca9181
Kauid323 <yuzi6666@outlook.com>
2025-10-05 13:13:18 +0800

    upload

--- app/src/main/java/com/yhchat/canary/MainActivity.kt
+++ app/src/main/java/com/yhchat/canary/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yhchat.canary.ui.login.LoginScreen
 import com.yhchat.canary.ui.conversation.ConversationScreen
 import com.yhchat.canary.ui.chat.ChatScreen
