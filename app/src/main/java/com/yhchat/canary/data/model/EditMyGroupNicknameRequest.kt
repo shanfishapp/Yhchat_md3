c4b3177fda6e2d0975b3c1bde19078dc80cb3b71
shanfishapp <zcsfish@qq.com>
2025-11-29 00:06:39 +0800

    修复api错误

--- app/src/main/java/com/yhchat/canary/data/model/EditMyGroupNicknameRequest.kt
+++ app/src/main/java/com/yhchat/canary/data/model/EditMyGroupNicknameRequest.kt
@@ -6,6 +6,6 @@ data class EditMyGroupNicknameRequest(
     @SerializedName("groupId")
     val groupId: String,
     
-    @SerializedName("botId")  // API使用botId字段来存储群昵称
+    @SerializedName("nickname")  // API使用botId字段来存储群昵称
     val botId: String
 )
